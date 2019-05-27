ca11712ee393655d927a3a41162da7cc93ba520a
Mickae Truman <mtruman@nyit.edu>
2019-05-27 23:15:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+    get '/' do
+    erb :user_input
+  end
+
+ post '/piglatinize' do
+  user_input = PigLatinizer.new
+ 
+  @piglatin = Piglatinizer.new(user_input)
+ 
+  erb :results
+end
+  
 end
